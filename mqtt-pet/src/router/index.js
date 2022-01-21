26803a851193f4389026b5ad3486c55bf255f762
Ilia Skakov <ilia.skakov@gmail.com>
2022-01-21 23:07:40 +0300

    added Vite template

---
+++ mqtt-pet/src/router/index.js
@@ -0,0 +1,15 @@
+import {createRouter, createWebHistory}  from 'vue-router'
+import HelloWorld from '../components/HelloWorld.vue'
+const routes = [
+    {
+        path: '/',
+        component: HelloWorld
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default router
---
+++ mqtt-pet/src/store/store.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+    state: {
+        title: "If you see this message,then Vuex is working perfect"
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+
+    }
+})
